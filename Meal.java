7175b597498d7ffd61edac84c413b84f3dc91e69
rhossain5420 <150310559+rhossain5420@users.noreply.github.com>
2023-12-06 13:32:25 -0500

    Fp2 file

---
+++ Meal.java
@@ -0,0 +1,59 @@
+
+// import java.util.List;
+
+class Meal {
+    // @SerializedName("strMeal")
+    private String strMeal;
+
+    // @SerializedName("strMealThumb")
+    private String strMealThumb;
+
+    // @SerializedName("idMeal")
+    private String idMeal;
+    
+// 
+//     public Meal(String strMeal, String strMealThumb, String idMeal) {
+//         this.strMeal = strMeal;
+//         this.strMealThumb = strMealThumb;
+//         this.idMeal = idMeal;
+//     }
+// 
+// 
+//     public String getStrMeal() {
+//         return strMeal;
+//     }
+// 
+// 
+//     public void setStrMeal(String strMeal) {
+//         this.strMeal = strMeal;
+//     }
+// 
+// 
+//     public String getStrMealThumb() {
+//         return strMealThumb;
+//     }
+// 
+// 
+//     public void setStrMealThumb(String strMealThumb) {
+//         this.strMealThumb = strMealThumb;
+//     }
+// 
+// 
+//     public String getIdMeal() {
+//         return idMeal;
+//     }
+// 
+// 
+//     public void setIdMeal(String idMeal) {
+//         this.idMeal = idMeal;
+//     }
+// 
+// 
+    @Override
+    public String toString() {
+        return "Meal" + "\n" +
+                "name = " + strMeal + "\n" +
+                "thumbnail = " + strMealThumb + "\n" +
+                "id = " + idMeal;
+    }
+}
