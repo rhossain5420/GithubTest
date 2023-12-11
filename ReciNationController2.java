f254f28a0f4f3b487e3405cd5cb2cde7611b7eb5
MDDesignMc3 <150461354+MDDesignMc3@users.noreply.github.com>
2023-12-11 12:24:19 -0500

    Con
    New Controller

---
+++ ReciNationController2.java
@@ -0,0 +1,39 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.ImageView;
+
+public class ReciNationController2 {
+
+    @FXML
+    private Label Instructions;
+
+    @FXML
+    private SplitPane Scene;
+
+    @FXML
+    private Label Title;
+
+    @FXML
+    private ImageView foodImage;
+
+    @FXML
+    private Button generateButton;
+
+    @FXML
+    private Hyperlink youTube;
+
+    @FXML
+    void clickVideo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateRecipes(ActionEvent event) {
+
+    }
+
+}
